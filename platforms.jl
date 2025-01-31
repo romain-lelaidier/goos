b67e63442514d3f5355a94432f8872f2ac49530d
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-01-31 12:09:01 +0100

    Ajout d'une fonction pour vérifier si 1 point se trouve à l'intérieur d'une plateforme

--- platforms.jl
+++ platforms.jl
@@ -23,3 +23,27 @@ function projection((x,y)::Tuple{Float64,Float64}, p::Platform)
 
     (res_x,res_y)
 end
+
+
+"""
+fonction in_platform((x,y)::Tuple{Float64, Float64}, p::Platform)
+
+renvoie true si la position (x,y) se trouve à l'intérieur de la plateforme p
+"""
+function in_platform((x,y)::Tuple{Float64,Float64}, p::Platform)
+    p.x_left < x < p.x_right && p.y_bottom < y < p.y_top
+end
+
+
+"""
+fonction link_check_platform((x1,y1)::Tuple{Float64,Float64}, 
+                             (x2,y2)::Tuple{Float64,Float64},
+                             p::Platform)
+
+Renvoie false si le lien entre (x1,y1) et (x2,y2) traverse la plateforme p
+"""
+function link_check_platform((x1,y1)::Tuple{Float64,Float64}, 
+                             (x2,y2)::Tuple{Float64,Float64},
+                             p::Platform)
+
+end
