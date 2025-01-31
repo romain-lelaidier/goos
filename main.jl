accc76633b651e3289e3d751423249d24ae2816a
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-01-31 11:06:51 +0100

    affichage d'un écran blanc

--- main.jl
+++ main.jl
@@ -1,28 +1,13 @@
 # imports
 using GLMakie
-GLMakie.activate!() # hide
-
-# modules internes
-using goosInterface
 
 
 
-goo = Observable{Goo}(0.,0.)
-
-
-# A MODIFIER
 scene = Scene(camera = campixel!)
-linesegments!(scene, points, color = :black)
-scatter!(scene, points, color = :gray)
 
-on(events(scene).mousebutton) do event
-    if event.button == Mouse.left
-        if event.action == Mouse.press || event.action == Mouse.release
-            mp = events(scene).mouseposition[]
-            push!(points[], mp)
-            notify(points)
-        end
-    end
-end
+goo_coords = 0.,0.
+goo = Observable{Tuple{Float64,Float64}}(goo_coords)
+
+scatter!(scene, goo_coords; color=:black)
 
 scene
