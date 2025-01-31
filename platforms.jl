0eb1f0cf1146b5a06d29fa8521b2ae2881098881
Noé Terrien <noeterrien@etu.minesparis.psl.eu>
2025-01-31 13:41:20 +0100

    Ajout de la fonction de vérification qu'un lien n'est pas à l'intérieur d'une plateforme

--- platforms.jl
+++ platforms.jl
@@ -1,3 +1,5 @@
+
+
 """Ce sont des rectangles, dont les coins sont donnés par:
     x_left, x_right, y_bottom, y_top"""
 struct Platform
@@ -28,11 +30,37 @@ end
 """
 fonction in_platform((x,y)::Tuple{Float64, Float64}, p::Platform)
 
-renvoie true si la position (x,y) se trouve à l'intérieur de la plateforme p
+renvoie true si la position (x,y) se trouve à l'intérieur de la plateforme p 
+(bords inclus)
 """
 function in_platform((x,y)::Tuple{Float64,Float64}, p::Platform)
-    p.x_left < x < p.x_right && p.y_bottom < y < p.y_top
+    p.x_left ≤ x ≤ p.x_right && p.y_bottom ≤ y ≤ p.y_top
 end
+
+
+"""
+	function segment_se_croisent(segment1, segment2)
+	
+Renvoie un booléen, indiquant si les segments se croisent. Les segments sont représenté par un couple/tableau de positions
+Les positions sont représentés par des couples ou des tableaux x,y
+"""
+function segment_se_croisent(segment1, segment2, bord_inclus=true)
+	#Cf brouillon
+	#On utilise la caractérisation du segement en at + (1-t) b
+	p1s1, p2s1 = segment1
+	p1s2, p2s2 = segment2
+	matA = hcat(collect(p1s1 .- p1s2), collect(p2s2 .- p1s2))
+	matB = collect(p2s2 .- p2s1)
+	#On résout matA X = mat B
+	x = matA \ matB
+
+	if bord_inclus
+		0 ≤ x[1] ≤ 1 && 0 ≤ x[2] ≤ 1
+	else
+		0 < x[1] < 1 && 0 < x[2] < 1
+	end
+end
+
 
 
 """
@@ -41,9 +69,15 @@ fonction link_check_platform((x1,y1)::Tuple{Float64,Float64},
                              p::Platform)
 
 Renvoie false si le lien entre (x1,y1) et (x2,y2) traverse la plateforme p
+/!\\ (x1,y1) et (x2,y2) sont supposés en dehors de la plateforme p et pas sur les bords/!\
+
 """
 function link_check_platform((x1,y1)::Tuple{Float64,Float64}, 
                              (x2,y2)::Tuple{Float64,Float64},
                              p::Platform)
-
+    !(
+    segment_se_croisent(((x1,y1),(x2,y2)),((p.x_left,p.y_bottom),(p.x_right,p.y_top)))
+    ||
+    segment_se_croisent(((x1,y1),(x2,y2)),((p.x_left,p.y_top),(p.x_right,p.y_bottom)))
+    )
 end
